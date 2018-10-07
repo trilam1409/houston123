60538bc7e9fd6127877bfcc9a2da6b8fadfc6d5d
huuphuoc1396 <huuphuoc1396@gmail.com>
2018-10-07 21:05:05 +0700

    Change route /api/users to /api/hocvien

--- routes/api.php
+++ routes/api.php
@@ -21,9 +21,9 @@ Route::group(['namespace' => 'api'], function () {
     Route::post('/login', 'UserController@login');
     Route::post('/register', 'UserController@register');
     Route::get('/logout', 'UserController@logout');
-   // Route::get('/user', 'UserController@user');
+
     Route::resource('giaovien', 'GiaovienController');
-    Route::resource('users', 'UserController');
+    Route::resource('hocvien', 'UserController');
     Route::resource('quanly', 'QuanlyController');
 });
 
